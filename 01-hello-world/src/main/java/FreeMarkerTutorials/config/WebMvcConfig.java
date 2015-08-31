cff294fcb5a0664ed29c1f1bc774766790cead27
Evangelia Dendramis <evangeliad@gmail.com>
2015-08-30 22:14:11 -0700

    removing unneeded settings, adding SessionLocaleResolver

--- 01-hello-world/src/main/java/FreeMarkerTutorials/config/WebMvcConfig.java
+++ 01-hello-world/src/main/java/FreeMarkerTutorials/config/WebMvcConfig.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
 import freemarker.template.TemplateException;
@@ -28,17 +29,10 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         CustomFreeMarkerViewResolver resolver = new CustomFreeMarkerViewResolver();
         resolver.setPrefix("/private/");
         resolver.setSuffix(".ftl");
-        resolver.setCache(false);
+        resolver.setCache(false); // don't disable the cache in production!
 
         resolver.setContentType("text/html;charset=UTF-8");
         resolver.setRequestContextAttribute("rc");
-        resolver.setExposeSpringMacroHelpers(true);
-        resolver.setExposeRequestAttributes(true);
-        resolver.setExposePathVariables(true);
-        resolver.setExposeSessionAttributes(true);
-        resolver.setExposeContextBeansAsAttributes(true);
-
-        resolver.setOrder(0);
 
         return resolver;
     }
@@ -51,22 +45,24 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         configurer.setServletContext(applicationContext.getServletContext());
         freemarker.template.Configuration configuration = configurer.createConfiguration();
         configuration.addAutoInclude("/templates/include-common.ftl");
-        configuration.setNumberFormat("0.######");
-        configuration.setWhitespaceStripping(true);
+        configuration.setServletContextForTemplateLoading(applicationContext.getServletContext(), "/WEB-INF/ftl/");
+        configuration.setIncompatibleImprovements(freemarker.template.Configuration.VERSION_2_3_23);
 
         configuration.setDefaultEncoding("UTF-8");
         configuration.setOutputEncoding("UTF-8");
         configuration.setURLEscapingCharset("UTF-8");
-        configuration.setServletContextForTemplateLoading(applicationContext.getServletContext(), "/WEB-INF/ftl/");
-        configuration.setTemplateUpdateDelay(0);
-
-        configuration.setLocale(Locale.ENGLISH);
-        configuration.setTagSyntax(configuration.ANGLE_BRACKET_TAG_SYNTAX); // ensure tags are angle brackets
-        configuration.setWhitespaceStripping(true);
-        configuration.setStrictBeanModels(false);
 
         configurer.setConfiguration(configuration);
 
         return configurer;
     }
+
+    @Bean
+    SessionLocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+
+        return localeResolver;
+    }
+
 }
